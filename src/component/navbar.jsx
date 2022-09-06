4d8d079e8f2b430d2b7de24e72da88074db32024
fathcode99 <fath970106@gmail.com>
2022-09-06 13:40:03 +0700

    read

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 
 import {
-    Container, Row, Col, Nav, Navbar, NavbarBrand
+    Nav, Navbar
 } from 'react-bootstrap'
 
 import { Link } from "react-router-dom";
--- src/pages/homepages.jsx
+++ src/pages/homepages.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Axios from 'axios'
 import {
-    Container, Row, Col, ThemeProvider
+    Container, Row, Col
 } from 'react-bootstrap'
 
 import './pages.css'
